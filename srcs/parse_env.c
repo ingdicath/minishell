f3f79eb40e4b39c05b193daee758a1f8af9b57ec
Diana catherine Salamanca leguizamon <dsalaman@f1r1s4.codam.nl>
2021-10-14 17:06:51 +0200

    Upload parse_env.c

--- srcs/parse_env.c
+++ srcs/parse_env.c
@@ -6,21 +6,21 @@
 /*   By: dsalaman <dsalaman@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/14 10:53:31 by dsalaman      #+#    #+#                 */
-/*   Updated: 2021/10/14 11:24:20 by dsalaman      ########   odam.nl         */
+/*   Updated: 2021/10/14 14:38:17 by dsalaman      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-typedef	struct s_env
+typedef struct s_env
 {
-	char	*key;
-	char	*value;
-	int		has_equal;
-	struct	s_env *next;
+	char			*key;
+	char			*value;
+	int				has_equal;
+	struct s_env	*next;
 }			t_env;
 
-t_env	*global_state;
+t_env	*g_state;
 
 void	parse_env(char **envp, t_env *state)
 {
@@ -33,7 +33,7 @@ void	parse_env(char **envp, t_env *state)
 	while (envp[i])
 	{
 		j = 0;
-		while(envp[i][j])
+		while (envp[i][j])
 		{
 			key = ft_substr(envp[i], 0, j);
 			value = ft_substr(envp[i], j + 1, ft_strlen(envp[i]) - j - 1);

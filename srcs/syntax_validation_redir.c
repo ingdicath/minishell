59c4371d96e8d692b754c2bea869a2367c40fa88
Huizhi Lin <hlin@f1r6s7.codam.nl>
2021-10-21 13:53:49 +0200

    sytax_validation

---
+++ srcs/syntax_validation_redir.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   syntax_validation_redir.c                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: hlin <hlin@student.codam.nl>                 +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/10/21 13:08:02 by hlin          #+#    #+#                 */
+/*   Updated: 2021/10/21 13:38:34 by hlin          ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+static int	skip_quotes(char *str, int i)
+{
+	if (str[i] == '"')
+	{
+		while (str[i] && str[i] != '"')
+			i++;
+	}
+	if (str[i] == '\'')
+	{
+		while (str[i] && str[i] != '\'')
+			i++;
+	}
+	return (i + 1);
+}
+
+static int	check_redir(char *str, int i)
+{
+	if ((str[i] == '>' && str[i + 1] == '>')
+		|| (str[i] == '<' && str[i + 1] == '<'))
+		i += 2;
+	else
+		i++;
+	while (str[i] == ' ')
+		i++;
+	if (!str[i] || str[i] == '|' || str[i] == '>' || str[i] == '<')
+	{
+		if (!str[i])
+			ft_putendl_fd
+				("minishell: syntax error near unexpected token `newline'", 2);
+		else
+		{
+			ft_putstr_fd("minishell: syntax error near unexpected token `", 2);
+			ft_putchar_fd(str[i], 2);
+			ft_putendl_fd("'", 2);
+		}
+		return (-1);
+	}
+	return (i);
+
+}
+
+int	redir_error(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '"' || str[i] == '\'')
+			i = skip_quotes(str, i);
+		while (str[i] == ' ')
+			i++;
+		if (str[i] == '>' || str[i] == '<')
+			i = check_redir(str, i);
+		if (i == -1)
+			return (1);
+		i++;
+	}
+	return (0);
